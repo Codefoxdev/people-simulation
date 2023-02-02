c30cb716cd0746440c5d81f1aecb4be3db05430d
CodeFoxDev <codefoxdev@outlook.com>
2023-02-02 12:56:54 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+import { loadImage } from "./func/sprite.js";
+import paths from "./func/paths.js";
+
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+const img = await loadImage("/Rijnstraat.png");
+
+ctx.fillStyle = "black"
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+ctx.drawImage(img, 0, 0, 800, 529);
+
+paths.draw(ctx);
